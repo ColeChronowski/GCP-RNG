d30bd097819ffaa38045a1d9dc4d5b7c827b4176
wyattscarpenter <wyattscarpenter@gmail.com>
2019-09-11 12:53:43 -0500

    add python appengine
    maximum iq plays

---
+++ Python/AppEngine/main_test.py
@@ -0,0 +1,10 @@
+#modified version of google's hello world tester
+
+import main
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
