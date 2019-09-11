91e0aac3257e3b33bc8a9ccd137a270aa066db70
Cole Chronowski <cole.chronowski@ou.edu>
2019-09-11 12:25:04 -0500

    Moved files to Java/AppEngine

---
+++ Java/AppEngine/src/main/java/myapp/DemoServlet.java
@@ -0,0 +1,21 @@
+package myapp;
+
+import java.lang.Math;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+  int MAX_NUMBER = 1000000;
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/html");
+    resp.getWriter().println(randomNumberGenerator());
+  }
+  private int randomNumberGenerator() {
+    return (int) Math.floor((Math.random() * MAX_NUMBER) + 1);
+  }
+}
