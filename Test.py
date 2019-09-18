3f1ed04b51c5b0237133a7612edd9bd7b431f921
Cole Chronowski <cole.chronowski@ou.edu>
2019-09-18 14:23:44 -0500

    Added testing for timing to Test.py and separated testing into different functions

--- Test.py
+++ Test.py
@@ -1,7 +1,9 @@
 import requests
+import time
 
 MAX_NUMBER = 1000000
 MIN_NUMBER = 1
+
 # Verify if the string is an integer or not
 def verify_number(s):
     try:
@@ -11,36 +13,62 @@ def verify_number(s):
     except ValueError:
         return False
 
-# Java URL
-url = 'https://civil-dolphin-251917.appspot.com/'
-# print("Java response:", requests.get(url).text)
-# Python URL
-# url = 'http://silent-region-214714.appspot.com/'
-# print("Python response:", requests.get(url).text)
-
-responses = set([])
-
-for test_num in range(10):
-    print("Test #" + str(test_num + 1))
-
-    num_same = 0
-
-    # get 1000 responses
-    for i in range(1000):
-        r = requests.get(url)
-        # verify that the response is a number
-        if verify_number(r.text):
-            if r.text in responses:
-                num_same += 1
-                print("Duplicate:", r.text)
-            else:
-                responses.add(r.text)
-        else:
-            print("Error:", r, "is not an integer")
-
-    # verify that at least 750 / 1000 of the random numbers are unique
-    if num_same <= 250:
-        print("Success!")
-    else:
-        print("Failure: Too many duplicates")
+# Test the randomness of 1000 random numbers. Fails if 250 or more requests are duplicates. This test is run 10 times in a row.
+def test_randomness(url):
+    # store the incoming numbers in a set
     responses = set([])
+
+    # conduct the test
+    for test_num in range(10):
+        print("Test #" + str(test_num + 1))
+        
+        # store the current number of duplicates for the current test
+        num_same = 0
+
+        # get 1000 responses
+        for i in range(1000):
+            r = requests.get(url)
+            # verify that the response is a number
+            if verify_number(r.text):
+                if r.text in responses:
+                    num_same += 1
+                    print("Duplicate:", r.text)
+                else:
+                    responses.add(r.text)
+            else:
+                print("Error:", r, "is not an integer")
+
+        # verify that at least 750 / 1000 of the random numbers are unique
+        if num_same <= 250:
+            print("Success!")
+        else:
+            print("Failure: Too many duplicates")
+        responses = set([])
+
+def test_timing():
+    # Array of tuples of urls. The first part of the tuple is the description of the url, and the second part of the tuple is the url itself.
+    # TODO: have these urls come from a YAML file
+    urls = [('Java App Engine', 'https://civil-dolphin-251917.appspot.com/'), 
+            ('Python App Engine', 'http://silent-region-214714.appspot.com/'),
+            ('Java Virtual Machine', 'http://35.188.106.110:8080/rng/'),
+            ('Python Virtual Machine', 'http://35.202.0.25/')]
+    
+    # Loop through the urls
+    for url in urls:
+        # Print the description of the url and the time
+        start = time.time()
+
+        # make the request
+        r = requests.get(url[1])
+
+        # get the end time
+        end = time.time()
+
+        # Verify that the request fits the constraints
+        if verify_number(r.text):
+            # print the results
+            print(url[0], (end - start), "seconds")
+        else:
+            print("An error has ocurred")
+
+test_timing()
